da047d7ea36d4e79eedef761ccb274559e3e63e3
Rebecca Novis <rebeccanovis@icloud.com>
2025-02-13 15:27:32 -0700

    Update UsernamePassword.js

--- src/authentication/strategy/UsernamePassword.js
+++ src/authentication/strategy/UsernamePassword.js
@@ -1,10 +1,12 @@
 import AuthenticationStrategyInterface from './AuthenticationStrategyInterface.js';
 
 class UsernamePassword extends AuthenticationStrategyInterface {
+    //should have constructure where it takes the database connection class, or it just takes the config. first option is better for OOP
+    //use ORM class for the constructor
     /**
      * concrete method
      */
-    authenticate(username, password) {
+    authenticate(params) {
         // TODO: SALT THE PASSWORD??
         // TODO: ORM 
         // TODO: RETURN THE USER (or true or false?)
