8fb065305785ed15845c16b1a0e9b04e112e18e2
JC Ho <leiadog11@gmail.com>
2025-03-03 17:18:04 -0700

    Finish Register

--- src/authentication/strategy/UsernamePassword.js
+++ src/authentication/strategy/UsernamePassword.js
@@ -7,23 +7,27 @@ const bcrypt = require('bcrypt'); // ??
 class UsernamePassword extends AuthenticationStrategyInterface {
     //should have constructor where it takes the database connection class, or it just takes the config. first option is better for OOP
     //use ORM class for the constructor
+
     /**
-     * concrete method
+     * Authenticates the user using username and password
+     * @param {*} params - username and password
+     * @returns true if localStorage items set
      */
     async authenticate(params) {
         // FUTURE ORM INSTEAD OF SQL
-        let user = await db.query("SELECT * from users WHERE username = ?", [params.username]);
+        let user = (await db.query("SELECT * from users WHERE username = ?", [params.username]))[0];
         if (!user) throw "User does not exist." // prompt to register?
 
         let isPassword = await bcrypt.compare(params.password, user.password);
         if (isPassword) {
             let userToken = jwt.sign(
-                { subject: user.id, clearance: user.control },
-                process.env.JWT_Secret,
+                { subject: user.id, clearance: user.clearance },
+                // CONFIG JWT SECRET ??,
                 { expiresIn: '6h' }
             );
-          // CREATE SESSION TOKEN ??  return { token: userToken, clearance: user.control };
-          // I assume every login strategy should return a login token back to the service, the service will then handle it
+            localStorage.setItem('token', userToken);
+            localStorage.setItem('clearance', user.clearance);
+            return true; // ?? what should I return? should it just reroute?
         }
         else {
             throw "Incorrect password."
@@ -31,32 +35,47 @@ class UsernamePassword extends AuthenticationStrategyInterface {
     }
 
     refreshAuthToken() {
+        // not needed? or needed for localStorage token?
         return;
     }
 
+    /**
+     * Register a new user with email and password
+     * @param {*} params - username, password, and email + anything else needed
+     */
     async registerUser(params) {
-        let user = await db.query("SELECT * from users WHERE username = ?", [params.username]);
-        if(user) {
+        let user = await db.query("SELECT * FROM users WHERE username = ?", [params.username]);
+        let password;
+        if(user[0].username) {
             throw "Username already taken."
+        }
+        if(user[0].email) {
+            throw "Email already has registered account." // prompt to login?
         }
 
         // apply salt with bcrypt
-        const saltRounds = 10;
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            bcrypt.hash(params.password, salt, function(err, hash) {
-                      // Store hash in database here
-             });
-        });
+        if (params.password) {
+            const saltRounds = 10;
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(params.password, salt, function(err, hash) {
+                    password = hash;
+                });
+            });
+        }
 
-        // login after created 
-        return;
+        let insertParams = [params.username, password, params.email];
+
+        await db.query("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", insertParams);
+
+        // log in after created
+        this.authenticate(insertParams);
     }
 
     logOutUser() {
-        // clear session and send to a home page
+        localStorage.clear();
+        // reroute???
         return;
     }
-  
 }
 
 export default UsernamePassword;
